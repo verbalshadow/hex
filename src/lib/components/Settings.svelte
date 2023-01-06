1f815838fd570207961c9dd6948aaedae2d12820
Joshua L. Blocher <verbalshadow@gmail.com>
2023-01-06 11:46:51 -0700

    update settings component.

--- src/lib/components/Settings.svelte
+++ src/lib/components/Settings.svelte
@@ -5,20 +5,20 @@
     // export let data: PageData;
 </script>
 
-<section class="card gap-4 m-4 p-4">
+<section class="card gap-4 m-4 p-4 flex">
     <form action="refresh">
-        <label for="refreshrat" class="block">
+        <label for="refreshrat" class="block m-2">
             <span>Refresh Rate</span> 
-        <select name="refreshrate" id="refreshrate" class="gap-4 p-4">
+        <select name="refreshrate" id="refreshrate" class="gap-4 flex-auto">
             {#each refreshTimes as rate } 
             <option value="{rate}">{rate} Secs</option>
             {/each}
         </select>
         </label>
-        <label for="oldest">
+        <label for="oldest" class="block m-2">
             <span>Oldest Date Pulled</span> 
-        <input type="datetime-local" name="oldest" id="oldest">
-        <input type="button" value="Save" class="btn btn-filled-success btn-xlg gap-4 m-4 bottom-0 right-0">
+        <input type="datetime-local" name="oldest" id="oldest" class="gap-4 flex-auto">
+        <input type="button" value="Save" class="btn btn-filled-success btn-xlg gap-4 m-4 justify-end float-right">
         </label>
     </form>
 </section>
